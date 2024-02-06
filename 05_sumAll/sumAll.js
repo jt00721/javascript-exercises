7aa77a88ac78bfe7bfc1a99a675894948863ccf1
jt00721 <titusgloverj@gmail.com>
2024-02-06 18:12:56 +0000

    Added two arguments to function, created variable to hold final sum, use for loop to iterate through each number from int1 to int2, add each number to result, check which argument is greater, check if arguments are negative, check if non-number argument was passed. Completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    //variable to hold result
+    //iterate through each number from int1 to int2
+    //add each number to result every loop
+    //return result
+    if (!(typeof(int1) === "number") || !(typeof(int2) === "number")) {
+        return "ERROR"
+    }
 
+    let isInt1Negative = int1 < 0;
+    let isInt2Negative = int2 < 0;
+
+    if (isInt1Negative || isInt2Negative) {
+        return "ERROR"
+    }
+    let result = 0;
+
+    let isInt1Bigger = int1 > int2;
+    if (isInt1Bigger) {
+        for (; int2 <= int1; int2++) {
+            result += int2;
+        }
+    } else {
+        for (; int1 <= int2; int1++) {
+            result += int1;
+        }
+    }
+    
+    
+    return result;
 };
 
 // Do not edit below this line
