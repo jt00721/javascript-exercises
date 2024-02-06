0058091a6e05f8989609f41a4106b80efa74e252
jt00721 <titusgloverj@gmail.com>
2024-02-06 18:45:12 +0000

    Added temp argument to each function, used conversion formula for each function, parsed float value for result of toFixed method. Completed 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  // number.toFixed(decimal_digits);
+  // F to C
+  let cTemp = (fTemp - 32) * (5/9);
+
+  return parseFloat(cTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  // number.toFixed(decimal_digits);
+  // C to F
+  let fTemp = (cTemp * (9/5) + 32);
+
+  return parseFloat(fTemp.toFixed(1));
 };
 
 // Do not edit below this line
