93224857b214c5974329e4cbb3b26eda4413134e
jt00721 <titusgloverj@gmail.com>
2024-03-03 13:14:30 +0000

    Added array of punctuation symbols, added a lower case version of str, put characters of lowercase str in an array, filtered out the spaces and punctuation symbols in the charactersOfStr, made the filtered array a string again, then reversed the string, then compare filteredStr to reversedStr to return a boolean value

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
 const palindromes = function (str) {
-    let reverseStr = str.split("").reverse().join("");
-    return str === reverseStr;
+    let punctuation = '.,/#!$%^&*;:{}=-_`~()'.split("");
+    let lowerCaseStr = str.toLowerCase();
+    let charactersOfStr = lowerCaseStr.split("");
+    let filteredCharactersOfStr = charactersOfStr.filter(character => character != " " && !(punctuation.includes(character)));
+    let filteredStr = filteredCharactersOfStr.join("");
+    let reverseStr = filteredCharactersOfStr.reverse().join("");
+    return filteredStr === reverseStr;
 };
 
 // Do not edit below this line
