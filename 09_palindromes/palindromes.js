8e128c88c9546dbb9af38b8cc95acc95d2b80b28
jt00721 <titusgloverj@gmail.com>
2024-03-03 12:21:23 +0000

    added str parameter for palindromes function, created a variable reverseStr, return boolean of str being equal to reverseStr.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let reverseStr = str.split("").reverse().join("");
+    return str === reverseStr;
 };
 
 // Do not edit below this line
