389ffbdfa777229931d1fdc441371d020fd55021
jt00721 <titusgloverj@gmail.com>
2024-03-04 13:08:15 +0000

    Added conditional to convert num parameter to number if num is notof type number.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,7 @@
 const fibonacci = function(num) {
+    if (typeof num !== "number") {
+        num = parseInt(num);
+    }
     if (num < 0) return "OOPS";
     if (num === 0) return 0;
     let fibArray = [0, 1];
