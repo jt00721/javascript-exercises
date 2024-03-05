1ca162fe3fe17d373e5a3e8d72b0c05645ae16a0
jt00721 <titusgloverj@gmail.com>
2024-03-05 14:28:14 +0000

    Added currentDate, obtained currentYear from currentDate, if yearOfDeath of person is empty then yearOfDeath for the person becomes the currentYear.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
 const findTheOldest = function(people) {
     const sortedByAge = people.sort(function(a, b) {
+        const currentDate = new Date();
+        let currentYear = currentDate.getFullYear();
+        if (!(a.yearOfDeath)){
+            a.yearOfDeath = Number(currentYear);
+        }
+        if (!(b.yearOfDeath)){
+            b.yearOfDeath = Number(currentYear);
+        }
         const lastPerson = a.yearOfDeath - a.yearOfBirth;
         const nextPerson = b.yearOfDeath - b.yearOfBirth;
         return lastPerson > nextPerson ? -1 : 1;
