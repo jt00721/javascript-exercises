c70c3100e3225afd3d7fd0a9a93bf54013683fbc
jt00721 <titusgloverj@gmail.com>
2024-03-05 14:11:07 +0000

    Added the people parameter to take in an object, sorted the people object from oldest to youngest, then return the oldest person.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const sortedByAge = people.sort(function(a, b) {
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+    });
+    return sortedByAge[0]
 };
 
 // Do not edit below this line
